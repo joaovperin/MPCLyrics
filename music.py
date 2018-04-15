79242b8322b535fc26c65cd4c1e4a82f9a97775e
João Perin <joaovperin@live.com>
2018-04-15 17:34:57 -0300

    Removed duplicated sources... Added Verbose -v Option and some improvements on the code

--- music.py
+++ music.py
@@ -6,11 +6,24 @@ import os
 import re
 import subprocess
 import time
-artist = " "
-song = " "
-def pegaMusica():
-    global song
-    global artist
+
+import argparse
+
+# Adds Command line args
+parser = argparse.ArgumentParser(description='Obtains an prints the lyric for the song playing on MPD/Mopidy.')
+parser.add_argument('--verbose', '-v', dest='verbose', action='count', help='Specify if needs to echo everything.')
+
+# Parses the Cmd Line Args
+args = parser.parse_args()
+
+# Prints if verbose above that level
+def printV(text):
+    if (args.verbose >= 1):
+        print("*** " + str(text))
+
+# Retorna a música e o arista tocando no momento
+def getCurrentPlayingSongAndArtist():
+    song, artist = '', ''
     ## Salva nome do artista
     os.system('mpc -f %artist% current > artist.txt')
     with open('artist.txt', 'r') as myfile:
@@ -20,43 +33,38 @@ def pegaMusica():
     os.system('mpc -f %title% current > song.txt')
     with open('song.txt', 'r') as myfile:
         song = myfile.read().replace('\n', '')
+    return [song, artist]
 
+# Busca e printa a letra de uma música dado o artista
+def printLyrics(song, artist):
+    # Limpa a tela e printa o nome da música
+    if (args.verbose == 0):
+        os.system('clear')
+    print("\n\n\n")
+    print("Current Song: '" + str(song) + "' - Current Artist: '" + str(artist) + "'.\n")
 
-def printaLetra():
-    global song
-    global artist
-    ##limpa tela
-    os.system('clear')
-    print("\n\n\n\n\n\n\n\n")
-    ## Printa nome da musica 
-    pegaMusica()
-    print(artist + " " +song + "\n")
-    ## Printa letra da musica
-    print(PyLyrics.getLyrics(artist,song))
+    lyrics = ''
+    try:
+        lyrics = PyLyrics.getLyrics(artist,song)
+    except TypeError:
+        lyrics = 'Song or Artist not found'
+    except:
+        lyrics = 'A error ocurred, sorry.'
+    # Printa letra da musica
+    print(lyrics)
 
+# Starts the application main loop
+printV ("Starting application...")
+song, artist, new_song, new_artist = '', '', '', ''
 
-def verificaMusica():
-    global song
-    global artist
-    ##Busca musica novamente e salva em outra variavel para comparacao
-    os.system('mpc -f %title% current > song.txt')
-    with open('song.txt', 'r') as myfile:
-        newsong=myfile.read().replace('\n', '')
-    
-    ##Verifica se a musica mudou e se mudou chama a funcao de printar as letras
-    if song != newsong:
-        song = newsong
-        printaLetra()
-        
-        
-
-
-# Verifica musica a cada 10 Segundos
+# Main Loop
 while True:
-    #print ("Verificando")
-    verificaMusica()
-    time.sleep(5)
-
-
-
-
+    (new_song, new_artist) = getCurrentPlayingSongAndArtist()
+    printV("Current Song: '" + str(new_song) + "' - Current Artist: '" + str(new_artist) + "'.")
+    # Verifica se a musica mudou e se mudou chama a funcao de printar as letra
+    if song != new_song and artist != new_artist:
+        printV("Changing song from '" + str(song) +"' to '" + str(new_song) + "'.")
+        (song, artist) = (new_song, new_artist)
+        printLyrics(song, artist)
+    time.sleep(3)
+#End.
